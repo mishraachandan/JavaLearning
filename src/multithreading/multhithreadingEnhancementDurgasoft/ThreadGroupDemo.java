e3334b940a8ab2be0c7b322309c0aeeb90394b37
Chandan Mishra <chandan345mishra@gmail.com>
2023-12-24 11:45:11 +0530

    new changes

--- src/multithreading/multhithreadingEnhancementDurgasoft/ThreadGroupDemo.java
+++ src/multithreading/multhithreadingEnhancementDurgasoft/ThreadGroupDemo.java
@@ -4,5 +4,9 @@ public class ThreadGroupDemo {
     public static void main(String[] args) {
         ThreadGroup t = new ThreadGroup("DummyGroup");
         System.out.println(t.getParent().getName());
+
+        ThreadGroup g1 = new ThreadGroup(t, "Sub-ThreadGroup");
+        System.out.println(g1.getParent().getName());  // output >> DummyGroup.
+
     }
 }
